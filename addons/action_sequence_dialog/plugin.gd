8c13dc7cdd40dee646f32d7d9ea2edeeae2ad3ab
Lyndon Tan <pikadon92@gmail.com>
2020-12-21 01:46:02 +0800

    Set up Plugin for Toolbars

--- addons/action_sequence_dialog/plugin.gd
+++ addons/action_sequence_dialog/plugin.gd
@@ -3,8 +3,21 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	pass
-
+	connect("scene_changed", self, "_on_scene_changed")
+	
+	# ADD TOOLBARS
+	#add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, control)
 
 func _exit_tree():
+	hide_plugin_toolbars()
+	
+	disconnect("scene_changed", self, "_on_scene_changed")
+
+func _on_scene_changed(new_root):
+	hide_plugin_toolbars()
+	
+	if new_root is Overworld:
+		print("Add toolbar")
+
+func hide_plugin_toolbars():
 	pass
