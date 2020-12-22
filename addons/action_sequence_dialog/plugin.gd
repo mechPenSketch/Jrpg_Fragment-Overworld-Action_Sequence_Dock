156cdc7537de960263d8bcf2f9cbaf1a73c3498e
Lyndon Tan <pikadon92@gmail.com>
2020-12-23 01:58:22 +0800

    Corrected issues with Additional Toolbar

--- addons/action_sequence_dialog/plugin.gd
+++ addons/action_sequence_dialog/plugin.gd
@@ -1,14 +1,23 @@
 tool
 extends EditorPlugin
 
-var additional_mains
+var additional_mains = []
 
 func _enter_tree():
 	connect("scene_changed", self, "_on_scene_changed")
 	
 	# ADD TO MAIN TOOLBAR
-	var scn_at = load("AdditionalToolbar.tscn")
-	additional_mains = scn_at.get_children()
+	#	ADD EVENT
+	var add_event = ToolButton.new()
+	add_event.set_button_icon(load("res://addons/action_sequence_dialog/additional_toolbar/add_event.svg"))
+	add_event.connect("pressed", self, "_on_add_event_pressed")
+	
+	#	ACION SEQUENCE
+	var action_sequence = ToolButton.new()
+	action_sequence.set_button_icon(load("res://addons/action_sequence_dialog/additional_toolbar/action_squence.svg"))
+	action_sequence.connect("pressed", self, "_on_action_sequence_pressed")
+	
+	additional_mains += [VSeparator.new(), add_event, action_sequence]
 	for c in additional_mains:
 		add_control_to_container(CONTAINER_CANVAS_EDITOR_MENU, c)
 
@@ -24,3 +33,9 @@ func _on_scene_changed(new_root):
 	
 	for c in additional_mains:
 		c.set_visible(root_is_Overworld)
+
+func _on_add_event_pressed():
+	pass
+
+func _on_action_sequence_pressed():
+	pass
