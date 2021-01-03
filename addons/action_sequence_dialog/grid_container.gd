633fd3eba66566645d6d14b733032f8fab999410
Lyndon Tan <pikadon92@gmail.com>
2021-01-03 16:29:16 +0800

    Adjust Grid's Column upon Resizing

---
+++ addons/action_sequence_dialog/grid_container.gd
@@ -0,0 +1,30 @@
+tool
+extends GridContainer
+
+const BTN_WIDTH = 32
+const PARENT_MARGIN = 16
+
+func _ready():
+	_on_dock_resized()
+
+func _on_dock_resized():
+	var new_width = int(find_parent("Dock").get_size().x)
+	new_width -= PARENT_MARGIN * 2
+	var hseparation = get("custom_constants/hseparation")
+	new_width += hseparation
+	var btn_width_w_hsep = BTN_WIDTH + hseparation
+	var final_column = new_width / btn_width_w_hsep
+	if final_column <= 1:
+		final_column = 1
+	
+	# IF THERE'S ENOUGH LEFTOVER SPACE TO FIT A BUTTON
+	print(new_width)
+	print(btn_width_w_hsep)
+	print(final_column)
+	print(new_width % btn_width_w_hsep)
+	print(new_width % btn_width_w_hsep >= BTN_WIDTH)
+	if new_width % btn_width_w_hsep >= BTN_WIDTH:
+		# ADD ONE MORE COLUMN
+		final_column += 1
+	
+	set_columns(final_column)
